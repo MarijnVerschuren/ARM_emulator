cf406265877488a8cbd12d79ffed18b7b56dbae7
MarijnVerschuren <marijnverschuren3@gmail.com>
2024-07-15 12:14:08 +0200

    Integration

--- emulator/hardware.py
+++ emulator/hardware.py
@@ -1,5 +1,6 @@
 # TUI includes
-from crc import Register
+from functools import cache
+from typing import Iterator
 from rich import print
 
 # custom includes
@@ -31,9 +32,9 @@ class Hardware:
 		for peripheral in self.dev:
 			for register in peripheral:
 				register.update()
-	def find_register(self, label: str, offset: int) -> Register:
+	def find_register(self, peripheral: str, offset: int) -> "Register":
 		for periph in self.dev:
-			if periph.label != label: continue
+			if periph.label != peripheral: continue
 			return periph[offset]
 
 	# hooks
@@ -54,8 +55,9 @@ class Hardware:
 			break
 		else: print(f"write {hex(value)} to {hex(address)}, size: {size}, access:{access}")
 
+
 class Peripheral:
-	def __init__(self, emu, type: str, regs: list[Register], label: str, base: int) -> None:
+	def __init__(self, emu, type: str, regs: list["Register"], label: str, base: int) -> None:
 		self.emu = emu
 		self.type = type
 		self.regs = regs
@@ -67,21 +69,21 @@ class Peripheral:
 	def __str__(self) -> str:	return f"<[{self.__class__.__name__}], type: {self.type}, label: {self.label}, base: {self.base}, regs: {self.regs}>"
 	def __repr__(self) -> str:	return f"<[{self.__class__.__name__}], {self.type}, {self.label}, {self.base}, {self.regs}>"
 
+	@cache
 	@property
 	def map_max(self) -> int: return max(self.regs, key=lambda x: x.offset).offset + 4
 	def offset(self, addr: int) -> tuple[bool, int]:
 		offset: int = addr - self.base
 		return 0 <= offset < self.map_max, offset
-	def read(self, offset: int, value: int) -> None:	self.map[offset].read(value)
-	def write(self, offset: int, value: int) -> None:	self.map[offset].write(value)
+	def __getitem__(self, offset: int) -> "Register":
+		for reg in self.regs:
+			if reg.offset == offset: return reg
+	def __iter__(self) -> Iterator["Register"]:
+		return iter(self.regs)
 
-	def __getitem__(self, offset: int) -> "Register":	return self.map[offset]
-	def __iter__(self) -> "Peripheral":					self.i = 0; return self
-	def __next__(self) -> tuple:
-		try:
-			data = self.regs[self.i]
-			self.i += 1; return data
-		except IndexError: raise StopIteration
+	# control
+	def read(self, offset: int, value: int) -> None:	self[offset].read(value)
+	def write(self, offset: int, value: int) -> None:	self[offset].write(value)
 
 
 class Register:
@@ -101,57 +103,48 @@ class Register:
 	def __str__(self) -> str:	return f"<[{self.__class__.__name__}], offset: {self.offset}, label: {self.label}, bits: {self.bits}, reset: {self.reset}, triggers: {self.triggers}>"
 	def __repr__(self) -> str:	return f"<[{self.__class__.__name__}], {self.offset}, {self.label}, {self.reset}, {self.triggers}>"
 
-
+	# control
 	def update(self) -> None:
 		self.emu.mem_write(self.ptr, self.value.to_bytes(4, byteorder="little"))
 
-	def action(self, action: dict) -> None:
-		dst = action["target"]
-		d_ptr = self.parent.base + dst["offset"]
-		for act, dat in action["action"].items():
-			if act == "copy":
-				c_ptr = self.parent.base + dat["offset"]
-				data = int.from_bytes(self.emu.mem_read(c_ptr, 4), byteorder="little")
-				dat = (data >> dat["bit_offset"]) & ((2 ** dat["count"]) - 1)
-			cval = int.from_bytes(self.emu.mem_read(d_ptr, 4), byteorder="little")
-			mask = ((2 ** dst["count"]) - 1); pos = dst["bit_offset"]
-			cval &= ~(mask << pos)
-			cval |= (dat & mask) << pos
-			print(f"act: {act}, res: {hex(cval)}, ptr: {hex(d_ptr)}, regs: {self.emu.read_regs()}")
-			if cval == 0xb3005: self.emu.single_step = True
-			self.emu.mem_write(d_ptr, cval.to_bytes(4, byteorder="little"))
-			# Write to self cannot be done because output is overwritten by code!!!!!!!
-			# NEW system: cache the output and write it on read
 	def read(self, value: int) -> None:
 		self.update()
 		print(f"read {hex(value)} from {self.parent.label}->{self.label}, regs: {self.emu.read_regs()}")
-		if not self.actions: return
-		for action in self.actions:
-			if action["trigger"] != "read": continue
-			self.action(action)
+		for trigger in self.triggers: trigger.read_hook(self)
+
 	def write(self, value: int) -> None:
 		print(f"wrote {hex(value)} to {self.parent.label}->{self.label}, regs: {self.emu.read_regs()}")
-		if not self.actions: return
-		for action in self.actions:
-			if action["trigger"] != "write": continue
-			src = action["source"]
-			val = value & ((2 ** src["count"]) - 1) << src["bit_offset"]
-			if not val and not ("setting" in action and (val >> src["bit_offset"]) == action["setting"]): continue
-			self.action(action)
+		for trigger in self.triggers: trigger.write_hook(self, value)
 
-#  TODO
+
 class Trigger:
 	def __init__(self, emu, actions: list["Action"], src: None or tuple[int, int, int or None] = None):
 		# write if src, else read
-		self.emu = emu
-		self.actions = actions
-		self.src = src		# bit, count, setting or any
+		self.emu =		emu
+		self.actions =	actions
+		self.src =		src		# bit, count, setting or any
 
 	# getters
 	def __str__(self) -> str:	return f"<[{self.__class__.__name__}], type: {self.type}, src: {self.src}, actions: {self.actions}>"
 	def __repr__(self) -> str:	return f"<[{self.__class__.__name__}], {self.type}, {self.src}, {self.actions}>"
 	@property
 	def type(self) -> str:		return ("write " + ("setting" if self.src[2] else "any")) if self.src else "read"
+
+	# control
+	def __call__(self, *args, **kwargs) -> None:
+		for action in self.actions: action()
+
+	def read_hook(self, reg: Register) -> None:
+		if not self.src: self()
+
+	def write_hook(self, reg: Register, value: int) -> None:
+		if not self.src:				return
+		o, c, s = self.src[2]
+		msk = ((2 ** c) - 1) << o
+		dat = value & msk
+		if not dat:						return
+		if s and not ((dat >> o) == s):	return
+		self()
 
 
 class Action:
@@ -166,3 +159,13 @@ class Action:
 	def __repr__(self) -> str:	return f"<[{self.__class__.__name__}], {self.type}, {self.src}, {self.dst}>"
 	@property
 	def type(self) -> str:		return "write" if isinstance(self.src, int) else "copy"
+
+	# control
+	def __call__(self, *args, **kwargs) -> None:
+		d_reg = self.emu.find_register(*self.dst[:2])
+		o, c = self.dst[2:]
+		if isinstance(self.src, int): return d_reg.write(self.src << o)
+		s_reg = self.emu.find_register(*self.src[:2])
+		msk = ((2 ** c) - 1) << o
+		d_reg.write(s_reg.value & msk)
+
